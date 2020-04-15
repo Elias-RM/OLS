35c4cff78b9caa6c4da0ff3ade02a0fd00221be1
elias reyes <eliasreyesmartinez7@gmail.com>
2020-04-14 23:48:36 -0500

    avance

--- tarea.py
+++ tarea.py
@@ -18,39 +18,59 @@ dataframe["Loutput_2"] = dataframe["Loutput"]**2
 dataframe['Lplabor'] = np.log(dataframe['plabor'])
 dataframe['Lpfuel'] = np.log(dataframe['pfuel'])
 dataframe['Lpkap'] = np.log(dataframe['pkap'])
+dataframe['Lpprod'] = np.dot(dataframe['plabor'] , dataframe['pfuel'])
+dataframe['Lptcostfuel'] = np.dot(dataframe['Loutput'] , dataframe['pfuel'])
 dataframe['One'] = 1
 print(dataframe.head(10))
+
 Y = dataframe["Ltotcost"]
 # que valores me faltan
-X = dataframe[["One","Loutput","Loutput_2", "Lplabor", "Lpfuel", "Lpkap"]]
+#X = [["One","Loutput","Loutput_2","Lplabor","Lpfuel", "Lpkap","Lpprod","Lptcostfuel"]]
+X = dataframe[["One","Loutput","Loutput_2", "Lplabor", "Lpfuel", "Lpkap","Lpprod","Lptcostfuel"]]
+
+#X = [["One","Loutput", "Lplabor", "Lpfuel", "Lpkap"]]
+
+
 est = sm.OLS(Y,X)
 est2 = est.fit()
 print(est2.summary())
-
+print("\n\n\n=====================\n   OLS\n=====================\n\n\n")
 print("\n Parametros \n ",est2.params)
 print("\n Valores \n", est2.tvalues)
 print("\n Fvalue \n", est2.fvalue)
 
-R = np.array(([0,0,0,0,0], [0,1,0,0,0], [0,0,1,0,0], [0,0,0,1,0], [0,0,0,0,1]))
+R = np.array(([0,0,0,0,0,0,0,0], [0,1,0,0,0,0,0,0], [0,0,1,0,0,0,0,0], [0,0,0,1,0,0,0,0], [0,0,0,0,1,0,0,0], [0,0,0,0,0,1,0,0], [0,0,0,0,0,0,1,0], [0,0,0,0,0,0,0,1] ))
 print(est2.f_test(R))
 
-formula = 'Ltotcost ~  One + Loutput + Loutput_2 + Lplabor + Lpfuel + Lpkap '
+formula = 'Ltotcost ~  One + Loutput  + Lplabor + Lpfuel + Lpkap + Lpprod + Lptcostfuel'
 #results = sm.OLS(formula, dataframe)
-print("\n\n\n")
+
 #formula = 'Ltotcost ~  One + Loutput + Lplabor + Lpfuel + Lpkap'
 results = ols(formula, dataframe).fit()
 
+print("\n\n\n=====================\n            Hipotesis 1\n=====================\n\n\n")
 hypotheses = 'Lplabor + Lpfuel + Lpkap = 1'
-
 t_test = results.t_test(hypotheses)
-print("\n****************** Hipotesis\n\n*************")
 print(t_test)
+f_test = results.f_test(hypotheses)
+print(f_test)
 
 # Otro ejemplo: 
-hypotheses_2 = 'Lplabor + Lpfuel + Lpkap = 1, Loutput = 0'
+print("\n\n\n=====================\n            Hipotesis 2\n=====================\n\n\n")
+hypotheses_2 = 'Lplabor + Lpfuel + Lpkap = 1, Lptcostfuel = 0'
+t_test = results.t_test(hypotheses_2)
+print(t_test)
 f_test = results.f_test(hypotheses_2)
 print(f_test)
 
+# Otro ejemplo: 
+print("\n\n\n=====================\n            Hipotesis 3\n=====================\n\n\n")
+hypotheses_2 = 'Lplabor + Lpfuel + Lpkap = 1, Lpprod = 0'
+hypotheses_2 = 'Lplabor + Lpfuel + Lpkap = 1, Lptcostfuel = 0'
+t_test = results.t_test(hypotheses_2)
+print(t_test)
+f_test = results.f_test(hypotheses_2)
+print(f_test)
 
 
 dataframe.to_csv('guardar.csv')
